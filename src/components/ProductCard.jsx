98e7e001e9046fe75f770f2716ca4a664ae610a4
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-09-22 20:10:38 +0500

    update UI

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,47 +1,46 @@
 import React from "react";
 import { productList } from "../data/products";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import {addItem} from "../Store/cartSlice"
+import {BsArrowUpShort} from "react-icons/bs"
 
 const ProductCard = () => {
-  const dispatch = useDispatch();
   const products = productList;
-
-  const onAddHandler = (product) => {
-    dispatch(addItem(product))
-  };
 
   return (
     <div className="flex flex-wrap items-center justify-center gap-6 py-8">
       {products?.map((item) => (
-        <div className="w-[250px] border rounded-lg overflow-hidden cursor-pointer bg-white shadow-sm" key={item.id}>
-          <div className="img-box border-b overflow-hidden">
-            <img
-              src={item.Img}
-              alt={item.Title}
-              className="w-[250px] hover:scale-[1.05] transition duration-300 "
-            />
-          </div>
+        <div
+          className="w-[270px] border rounded-lg overflow-hidden cursor-pointer bg-white shadow-sm relative"
+          key={item.id}
+        >
 
-          <div className="desc p-4 flex items-center justify-between">
-            <Link to={`products/${item.id}`}>
-              <h1 className="text-lg font-medium text-[#555] hover:underline">
-                {item.Cat}
-              </h1>
-            </Link>
-            <span className="text-md font-medium text-indigo-500">
-              $ {item.Price}
-            </span>
-          </div>
+        <div className="card-overlay absolute top-2 right-2 w-[30px] h-[30px] z-10 overflow-hidden">
+          <BsArrowUpShort size={30} className="text-[#777] rotate-[45deg] hover:scale[1.05]" />
+        </div>
 
-          <div className="add-to-cart-btn w-full text-center p-3">
-            <button
-              onClick={() => onAddHandler(item)}
-              className="w-full border p-2 bg-[#111] text-white rounded-md hover:bg-[#222]"
-            >
-              Add to cart
-            </button>
+          <div className="w-full">
+            <div className="img-box border-b overflow-hidden">
+              <img
+                src={item.Img}
+                alt={item.Title}
+                className="w-[250px] hover:scale-[1.05] transition duration-300 "
+              />
+            </div>
+
+            <div className="desc p-4 flex items-center justify-between">
+              <Link to={`products/${item.id}`}>
+                <h1 className="text-lg font-medium text-[#555] hover:underline">
+                  {item.Cat}
+                </h1>
+              </Link>
+              <span className="text-md font-medium text-indigo-500">
+                $ {item.Price}
+              </span>
+            </div>
+
+            <div className="w-full title px-4 pb-4">
+              <h2 className="text-md text-[#777]">{item.Title}</h2>
+            </div>
           </div>
         </div>
       ))}
--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem } from "../../Store/cartSlice";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const dispatch = useDispatch();
+  const cartProducts = useSelector((state) => state.cart);
+  const itemRemoveHandler = (productId) => {
+    dispatch(removeItem(productId));
+  };
 
-export default Cart
+  return (
+    <div className="w-full px-[30px]">
+      <div className="cart-title py-8">
+        <h1 className="w-full text-left text-2xl font-semibold text-[--text-color]">
+          Your Cart Items
+        </h1>
+      </div>
+
+      <div className="cart-items w-full border">
+        {cartProducts.map((item) => (
+          <div
+            className="w-full flex items-center gap-8"
+            key={item.id}
+          >
+            <div className="img">
+              <img src={item.Img} alt={item.Cat} className="w-[180px]" />
+            </div>
+
+            <div className="title flex flex-col gap-2 text-left">
+              <h1 className="text-xl font-semibold text-[#333]">{item.Cat}</h1>
+              <p className="text-lg font-semibold text-[#777]">${item.Price}</p>
+            </div>
+
+            <div className="removeBtn">
+              <button
+                onClick={() => itemRemoveHandler(item.id)}
+                className="border-2 p-2 border-red-400"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
